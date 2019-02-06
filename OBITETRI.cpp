12cc4457d1eae4e34894291d6ba848057951c425
Reverse_Neto <mathiconical@users.noreply.github.com>
2019-02-06 02:03:27 -0200

    Update OBITETRI.cpp

--- OBITETRI.cpp
+++ OBITETRI.cpp
@@ -1,98 +1,59 @@
-// lib manipular strings
 #include <cstring>
-// lib entrada / saida
 #include <iostream>
 
-using namespace std;
+struct Jogador
+{
+  std::string nome;
+  int pontos = 0;
+  int menor = 9999;
+  int maior = 0;
+};
 
-int main(void){ // Pontuacao jogo Tetris;
+int main()
+{
+  int t = 1;
+  int totalJogadores;
+  std::cout << "Quantidade de jogadores: ";
+  std::cin >> totalJogadores;
 
-    int jogador; // variavel jogador
-    int  i, k, teste=1; // variaveis de controle
+  Jogador jogadores[totalJogadores];
 
-    while(true){ // main loop
+  for(int i = 0; i < totalJogadores; i++)
+  {
+    std::cout << "Digite o nome: ";
+    std::cin >> jogadores[i].nome;
 
-//        cout << "Entre a quantidade de jogadores.\n";    
-        cin >> jogador; // quantidade de jogadores
+    int pontos = 0;
+    for(int j = 0; j < 12; j++)
+    {
+      std::cin >> pontos;
+      if(jogadores[i].menor > jogadores[i].pontos){ jogadores[i].menor = jogadores[i].pontos; }
+      if(jogadores[i].maior < jogadores[i].pontos){ jogadores[i].maior = jogadores[i].pontos; }
+      jogadores[i].pontos += pontos;
+    }
+    jogadores[i].pontos = jogadores[i].pontos - (jogadores[i].maior + jogadores[i].menor);
+  }
 
-        if(jogador<=0 or jogador > 1000) // se jogador for igual a 0
-            break; // sai do programa
-        
-        string nome[jogador]; // string por 'n' jogadores
-        
-        char nomes[20]; // variavel para pegar o nome do jogador;
-          
-        int pontos[jogador]; // array para armazenar pontos por 'n' jogadores
+  for(int k = 0; k < totalJogadores - 1; k++)
+  {
+    for(int l = k + 1; l < totalJogadores; l++)
+    {
+      if(jogadores[k].pontos < jogadores[l].pontos)
+        std::swap(jogadores[k], jogadores[l]);
 
-        /* BLOCO DE ENTRADA DE DADOS */
-    
-        for(i=0;i<jogador;i++){ // loop para nome
-//            cout << "Entre com " << i+1 << " nome do jogador.\n";
-//            cin >> nome[i]; // entrada do nome em 'i'
-            cin >> nomes; // entrada com o nome do jogador de acordo com 'i' no loop for
-            if(strlen(nomes)>15) // verifica se nome possui mais de 15 caracteres
-                break; // sai do programa caso tenha
-                
-            int soma=0; // variavel para somar as entradas dos pontos
-            int menor=1001; // variavel para pegar o menor numero
-            int maior=-1; // variavel para pegar o maior numero
-            int ponto; // variavel para solicitar entrada dos pontos
-
-            for(k=0;k<12;k++){ // loop para ponto
-//                cout << "Entre com o " << k+1 << " ponto de " << nome[i] << ".\n";
-                cin >> ponto; // entrada dos pontos em 'k'
-                soma+= ponto; // soma recebe valores de temp em 'k'
-
-                if(maior < ponto){ // verifica se maior < ponto em 'k'
-                    maior = ponto; // se sim, maior = ponto em 'k'
-//                    cout << "O maior ponto de " << nome[i] << " foi " << maior << ".\n";
-                }
-                if(menor > ponto){ // verifica se menor > ponto em 'k'
-                    menor = ponto; // se sim, menor = ponto em 'k'
-//                    cout << "O menor ponto de " << nome[i] << " foi " << menor << ".\n";
-                }
-            }
-            pontos[i] = soma - (maior+menor); // novo valor sem o menor e maior numero
-//            cout << "A pontuacao apos menor e maior e de " << pontos[i] << ".\n";
-            nome[i] = nomes; // string recebe nome apos verificacao;
-        }
-    
-        /* BLOCO DE COMPARACAO DOS RESULTADOS */
-        /*    Nesse bloco usei Bubble Sort    */
-        /*    Juntamente com a funcao swap    */
-
-        for(int i=0;i<jogador;i++){ // permuta de i ate jogador
-            for(int k=0;k<jogador-1;k++){ // permuta de k ate jogador-1
-                if(pontos[k] < pontos[k+1]){ // verifica se pontos de k eh menor que ponto de k+1
-                    swap(nome[k], nome[k+1]); // uso da swap para trocar o nome de k+1 para k
-                    swap(pontos[k], pontos[k+1]); // uso da swap para trocar o ponto de k+1 para k
-                }
-                if(pontos[k] == pontos[k+1]){ // verifica se pontos sao iguais
-                    if(nome[k] > nome[k+1]){ // verifica se a Letra de K for maior que a Letra de K+1
-                        swap(nome[k], nome[k+1]); // entao troca de K para K+1
-                    }
-                }
-            }
-        }
-
-        /* BLOCO DE CRIACAO DO RANKING */
-        
-        int pos[jogador]; // definindo um array com tamanho da quantidade de jogadores
-        for(int i=0;i<jogador;i++) // permuta entre 0 ate jogador-1
-            pos[i] = i+1; // ja cria uma pos setada independente de acordo com a quantidade de jogadores
-        
-        // Este for ira comparar a pontuacao e setar a pos identica para tais
-        for(int i=0;i<jogador-1;i++) // permuta do loop de 0 ate jogador-2
-            if(pontos[i] == pontos[i+1]) // verifica se a pontuacao em 'i' eh igual em 'i+1'
-                pos[i+1] = pos[i]; // caso seja, pos de 'i+1' recebe a pos de 'i' que eh a anterior a ele
-        
-        /* BLOCO DE SAIDA DO RANKING FINAL */
-    
-        cout << "Teste " << teste << '\n'; // saida do caso de teste em N
-        for(int i=0;i<jogador;i++) // for para ler o ranking no tamanho de jogador
-            cout << pos[i] << "  " << pontos[i] << "  " << nome[i] << "\n"; // cout com tabulacoes (tab)
-        
-  	teste++; // incremento da variavel teste
-	}
-    return 0;
+      if(jogadores[k].pontos == jogadores[l].pontos)
+        if(jogadores[k].nome > jogadores[l].nome)
+          std::swap(jogadores[k], jogadores[l]);
+    }
+    std::cout << "Teste " << t << '\n';
+    for(int i = 0; i < totalJogadores; i++)
+      std::cout << i + 1 << " " << jogadores[i].pontos
+                << " " << jogadores[i].nome << "\n";
+      //std::cout << "NOME: " << jogadores[i].nome << '\n'
+      //          << "PONTOS: " << jogadores[i].pontos << "\n\n";
+    t++;
+  }
+// int aux;
+// std::cin >> aux;
+  return 0;
 }
